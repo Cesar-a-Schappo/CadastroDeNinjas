e93f4e7060c0967118b1317837bd8a1bd661fd55
Megacaquito <cesinha.schappo07@gmail.com>
2025-09-17 15:27:17 -0300

    feat: adiciona controller, repository e service para os ninjas e as missoes

---
+++ src/main/java/dev/Java10x/CadastroDeNinjas/Missions/MissionsRepository.java
@@ -0,0 +1,6 @@
+package dev.Java10x.CadastroDeNinjas.Missions;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MissionsRepository extends JpaRepository<MissionsModel, Long> {
+}
