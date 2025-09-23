46b8c640e0ca469eb6510534f704bcf836ae5192
Megacaquito <cesinha.schappo07@gmail.com>
2025-09-23 19:58:13 -0300

    feat: Cria as rotas do controller de missions com mock

--- src/main/java/dev/Java10x/CadastroDeNinjas/Missions/MissionsController.java
+++ src/main/java/dev/Java10x/CadastroDeNinjas/Missions/MissionsController.java
@@ -1,4 +1,33 @@
 package dev.Java10x.CadastroDeNinjas.Missions;
 
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("missions")
 public class MissionsController {
+
+    // Get -- Mandar uma requisicao para mostrar as missoes
+    @GetMapping("/list")
+    public String listMission() {
+        return "Missoes listadas com sucesso";
+    }
+
+    // Post -- Mandar uma requisicao para criar as missoes
+    @PostMapping("/create")
+    public String createMission() {
+        return "Missao criada com sucesso";
+    }
+
+    // Put -- Mandar uma requisicao para alterar as missoes
+    @PutMapping("/change")
+    public String changeMission() {
+        return "Missao alterada com sucesso";
+    }
+
+    // delete -- Mandar uma requisicao para deletar as missoes
+    @DeleteMapping("/delete")
+    public String deleteMission() {
+        return "Missao deletada com sucesso";
+    }
+
 }
