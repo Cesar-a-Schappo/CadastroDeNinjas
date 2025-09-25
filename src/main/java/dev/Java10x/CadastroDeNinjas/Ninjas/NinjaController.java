8909e31f19e99b7865f377175b14bbc322a7b041
Megacaquito <cesinha.schappo07@gmail.com>
2025-09-25 13:33:05 -0300

    fix: Tira a responsabilidade do meu Model e passa para dentro do meu DTO, usando streams no Mapper

--- src/main/java/dev/Java10x/CadastroDeNinjas/Ninjas/NinjaController.java
+++ src/main/java/dev/Java10x/CadastroDeNinjas/Ninjas/NinjaController.java
@@ -8,7 +8,7 @@ import java.util.List;
 @RequestMapping("ninjas")
 public class NinjaController {
 
-    private NinjaService ninjaService;
+    private final NinjaService ninjaService;
 
     public NinjaController(NinjaService ninjaService) {
         this.ninjaService = ninjaService;
@@ -27,19 +27,19 @@ public class NinjaController {
 
     // Mostrar todos os ninjas (READ)
     @GetMapping("/list")
-    public List<NinjaModel> listAllNinjas() {
+    public List<NinjaDTO> listAllNinjas() {
         return ninjaService.listAllNinjas();
     }
 
     // Mostrar ninja por ID (READ)
     @GetMapping("/list/{id}")
-    public NinjaModel listNinjaById(@PathVariable Long id) {
+    public NinjaDTO listNinjaById(@PathVariable Long id) {
         return ninjaService.listNinjaById(id);
     }
 
     // Alterar dados dos ninjas (UPDATE)
     @PutMapping("/update/{id}")
-    public NinjaModel updateNinjaById(@PathVariable Long id, @RequestBody NinjaModel updatedNinja) {
+    public NinjaDTO updateNinjaById(@PathVariable Long id, @RequestBody NinjaDTO updatedNinja) {
         return ninjaService.updateNinja(id, updatedNinja);
     }
 
--- src/main/java/dev/Java10x/CadastroDeNinjas/Ninjas/NinjaService.java
+++ src/main/java/dev/Java10x/CadastroDeNinjas/Ninjas/NinjaService.java
@@ -4,12 +4,13 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class NinjaService {
 
-    private NinjaRepository ninjaRepository;
-    private NinjaMapper ninjaMapper;
+    private final NinjaRepository ninjaRepository;
+    private final NinjaMapper ninjaMapper;
 
     public NinjaService(NinjaRepository ninjaRepository, NinjaMapper ninjaMapper) {
         this.ninjaRepository = ninjaRepository;
@@ -24,21 +25,27 @@ public class NinjaService {
     }
 
     // Listar todos os ninjas
-    public List<NinjaModel> listAllNinjas() {
-        return ninjaRepository.findAll();
+    public List<NinjaDTO> listAllNinjas() {
+        List<NinjaModel> ninjas = ninjaRepository.findAll();
+        return ninjas.stream()
+                .map(ninjaMapper::map)
+                .toList();
     }
 
     // Listar ninjas por ID
-    public NinjaModel listNinjaById(Long id) {
+    public NinjaDTO listNinjaById(Long id) {
         Optional<NinjaModel> ninjaById = ninjaRepository.findById(id);
-        return ninjaById.orElse(null);
+        return ninjaById.map(ninjaMapper::map).orElse(null);
     }
 
     // Atualizar ninja
-    public NinjaModel updateNinja(Long id, NinjaModel updatedNinja) {
-        if (ninjaRepository.existsById(id)) {
+    public NinjaDTO updateNinja(Long id, NinjaDTO ninjaDTO) {
+        Optional<NinjaModel> ninjaToUpdate = ninjaRepository.findById(id);
+        if (ninjaToUpdate.isPresent()) {
+            NinjaModel updatedNinja = ninjaMapper.map(ninjaDTO);
             updatedNinja.setId(id);
-            return ninjaRepository.save(updatedNinja);
+            NinjaModel savedNinja = ninjaRepository.save(updatedNinja);
+            return ninjaMapper.map(savedNinja);
         }
         return null;
     }
